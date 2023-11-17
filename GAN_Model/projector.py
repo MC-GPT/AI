e8be7fe1dd9b8a022308f28dcd405a7f32bfa690
wild-turkey <jhkimlego@naver.com>
2023-11-17 22:23:23 +0900

    modify inversion code

--- GAN_Model/projector.py
+++ GAN_Model/projector.py
@@ -1,32 +1,17 @@
-import argparse
-import math
+import torch
 import os
 from pathlib import Path
-
-import torch
-import torch.nn as nn
-from torch import optim
-from torch.nn import functional as F
-import pickle
-from torchvision import transforms
-from PIL import Image
-from tqdm import tqdm
-
 import lpips
 from model import Generator
 from op import fused_leaky_relu
 import numpy as np
 from util import *
-import torchvision
-from crop import img_crop 
+from torchvision import transforms
+from PIL import Image
+from tqdm import tqdm
 
-os.environ['KMP_DUPLICATE_LIB_OK']='True'
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
-
-def gaussian_loss(v):
-    # [B, 9088]
-    loss = (v-gt_mean.to('mps')) @ gt_cov_inv.to('mps') @ (v-gt_mean.to('mps')).transpose(1,0)
-    return loss.mean()
 
 def noise_regularize(noises):
     loss = 0
@@ -36,12 +21,13 @@ def noise_regularize(noises):
 
         while True:
             loss = (
-                loss
-                + (noise * torch.roll(noise, shifts=1, dims=3)).mean().pow(2)
-                + (noise * torch.roll(noise, shifts=1, dims=2)).mean().pow(2)
+                    loss
+                    + (noise * torch.roll(noise, shifts=1, dims=3)).mean().pow(2)
+                    + (noise * torch.roll(noise, shifts=1, dims=2)).mean().pow(2)
             )
 
-            if size <= 8: break 
+            if size <= 8:
+                break
             noise = noise.reshape([1, 1, size // 2, 2, size // 2, 2])
             noise = noise.mean([3, 5])
             size //= 2
@@ -73,42 +59,28 @@ def latent_noise(latent, strength):
 
 def make_image(tensor):
     return (
-        tensor.detach()
-        .clamp_(min=-1, max=1)
-        .add(1)
-        .div_(2)
-        .mul(255)
-        .type(torch.uint8)
-        .permute(0, 2, 3, 1)
-        .to('cpu')
-        .numpy()
+            tensor.detach()
+            .clamp_(min=-1, max=1)
+            .add(1)
+            .div_(2)
+            .mul(255)
+            .type(torch.uint8)
+            .permute(0, 2, 3, 1)
+            .to('cpu')
+            .numpy()
     )
 
 
-if __name__ == '__main__':
+def image_inversion(input_path, output_path):
     device = 'mps'
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--size', type=int, default=256)
-    parser.add_argument('--lr_rampup', type=float, default=0.05)
-    parser.add_argument('--lr_rampdown', type=float, default=0.25)
-    parser.add_argument('--lr', type=float, default=0.05)
-    parser.add_argument('--noise', type=float, default=0.1)
-    parser.add_argument('--noise_ramp', type=float, default=0.75)
-    parser.add_argument('--step', type=int, default=3000)
-    parser.add_argument('--noise_regularize', type=float, default=1e5)
-    parser.add_argument('--n_mean_latent', type=int, default=10000)
-    parser.add_argument('files', type=Path)
-
-    args = parser.parse_args()
 
     out_dir = './inversion_codes'
     os.makedirs(out_dir, exist_ok=True)
 
-    face_img = Image.fromarray(img_crop(args.files))
+    face_img = input_path
 
-    n_mean_latent = args.n_mean_latent
-    resize = min(args.size, 256)
+    n_mean_latent = 10000  # You can adjust this value
+    resize = 256
 
     transform = transforms.Compose(
         [
@@ -121,14 +93,13 @@ if __name__ == '__main__':
 
     imgs = []
 
-    img = transform(face_img)
+    img = transform(Image.open(face_img).convert('RGB'))
     imgs.append(img)
 
     imgs = torch.stack(imgs, 0).to(device)
 
-    g_ema = Generator(args.size, 512, 8)
-    ensure_checkpoint_exists('Models/face.pt')
-    g_ema.load_state_dict(torch.load('Models/face.pt', map_location='mps')['g_ema'], strict=False)
+    g_ema = Generator(256, 512, 8)
+    g_ema.load_state_dict(torch.load('/Users/hoon/Desktop/AI/GAN_Model/Models/face.pt', map_location='mps')['g_ema'], strict=False)
     g_ema = g_ema.to(device).eval()
 
     with torch.no_grad():
@@ -138,7 +109,7 @@ if __name__ == '__main__':
     # get gaussian stats
     if not os.path.isfile('inversion_stats.npz'):
         with torch.no_grad():
-            source = list2style(g_ema.get_latent(torch.randn([10000, 512],device='mps'))).cpu().numpy()
+            source = list2style(g_ema.get_latent(torch.randn([10000, 512], device='mps'))).cpu().numpy()
             gt_mean = source.mean(0)
             gt_cov = np.cov(source, rowvar=False)
 
@@ -147,25 +118,29 @@ if __name__ == '__main__':
         np.savez('inversion_stats.npz', mean=gt_mean, cov=gt_cov)
 
     data = np.load('inversion_stats.npz')
-    gt_mean = torch.tensor(data['mean'],device='mps').view(1,-1).float()
-    gt_cov_inv = torch.tensor(data['cov'],dtype=torch.float32)
+    gt_mean = torch.tensor(data['mean'], device='mps').view(1, -1).float()
+    gt_cov_inv = torch.tensor(data['cov'], dtype=torch.float32)
 
     # Only take diagonals
     mask = torch.eye(*gt_cov_inv.size())
-    gt_cov_inv = torch.inverse(gt_cov_inv*mask).float()
-
+    gt_cov_inv = torch.inverse(gt_cov_inv * mask).float()
 
     percept = lpips.LPIPS(net='vgg', spatial=True).to(device)
     latent_in.requires_grad = True
 
-    optimizer = optim.Adam([latent_in], lr=args.lr, betas=(0.9, 0.999))
+    optimizer = torch.optim.Adam([latent_in], lr=0.05, betas=(0.9, 0.999))
 
-    pbar = tqdm(range(args.step))
+    pbar = tqdm(range(3000))
     latent_path = []
 
+    def gaussian_loss(v):
+        # [B, 9088]
+        loss = (v - gt_mean.to('mps')) @ gt_cov_inv.to('mps') @ (v - gt_mean.to('mps')).transpose(1, 0)
+        return loss.mean()
+
     for i in pbar:
-        t = i / args.step
-        lr = get_lr(t, args.lr)
+        t = i / 3000
+        lr = get_lr(t, 0.05)
         latent_n = latent_in
 
         img_gen, _ = g_ema(style2list(latent_n))
@@ -173,11 +148,11 @@ if __name__ == '__main__':
         batch, channel, height, width = img_gen.shape
 
         if height > 256:
-            img_gen = F.interpolate(img_gen, size=(256,256), mode='area')
+            img_gen = F.interpolate(img_gen, size=(256, 256), mode='area')
 
-        p_loss = 20*percept(img_gen, imgs).mean()
-        mse_loss = 1*F.mse_loss(img_gen, imgs)
-        g_loss = 1e-3*gaussian_loss(latent_n.to('mps'))
+        p_loss = 20 * percept(img_gen, imgs).mean()
+        mse_loss = 1 * F.mse_loss(img_gen, imgs)
+        g_loss = 1e-3 * gaussian_loss(latent_n.to('mps'))
 
         loss = p_loss + mse_loss + g_loss
 
@@ -190,8 +165,8 @@ if __name__ == '__main__':
 
         pbar.set_description(
             (
-                f'perceptual: {p_loss.item():.4f};'
-                f' mse: {mse_loss.item():.4f}; gaussian: {g_loss.item():.4f} lr: {lr:.4f}'
+                    f'perceptual: {p_loss.item():.4f};'
+                    f' mse: {mse_loss.item():.4f}; gaussian: {g_loss.item():.4f} lr: {lr:.4f}'
             )
         )
 
@@ -200,13 +175,13 @@ if __name__ == '__main__':
     latent_path.append(latent_in.detach().clone())
     img_gen, _ = g_ema(style2list(latent_path[-1]))
 
-    filename = f'{out_dir}/{os.path.splitext(os.path.basename(args.files))[0]}.pt'
+    filename = f'{out_dir}/{os.path.splitext(os.path.basename(input_path))[0]}.pt'
 
     img_ar = make_image(img_gen)
 
-    real_file = str(args.files)
+    real_file = str(input_path)
     '''
     for i, input_name in enumerate(real_file):
         result_file['latent'] = latent_in[i]
     '''
-    torch.save({'latent': latent_in}, filename)
+    torch.save({'latent': latent_in}, output_path)
