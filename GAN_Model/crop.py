2f417a35799084dd27a2c4fc7e13da423c25eee5
wild-turkey <jhkimlego@naver.com>
2023-11-17 22:20:22 +0900

    modify crop code

--- GAN_Model/crop.py
+++ GAN_Model/crop.py
@@ -1,23 +1,19 @@
 import math
 import ntpath
-
 import tensorflow as tf
 from retinaface import RetinaFace
 import numpy as np
 from PIL import Image
 import cv2
 
-def img_crop(img_path):
+def crop_image(input_path, output_folder):
     cpus = tf.config.experimental.list_physical_devices('cpu')
     for cpu in cpus:
         tf.config.experimental.set_memory_growth(cpu, True)
 
-    imgfile = str(img_path)
+    resp = RetinaFace.detect_faces(input_path)
 
-    #codes for image cropping
-    resp = RetinaFace.detect_faces(imgfile)
-
-    img = cv2.imread(imgfile)
+    img = cv2.imread(input_path)
     img = img[:, :, ::-1]
 
     facial_area = resp['face_1']['facial_area']
@@ -25,12 +21,10 @@ def img_crop(img_path):
     left_eye = landmarks["left_eye"]
     right_eye = landmarks["right_eye"]
     nose = landmarks["nose"]
-    mouth_right = landmarks["mouth_right"]
-    mouth_left = landmarks["mouth_left"]
 
-    face_img = img[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]    
+    face_img = img[facial_area[1]: facial_area[3], facial_area[0]: facial_area[2]]
 
-        # swap
+    # swap
     left_eye, right_eye = right_eye, left_eye
 
     left_eye_x, left_eye_y = left_eye
@@ -40,31 +34,28 @@ def img_crop(img_path):
 
     if left_eye_y > right_eye_y:
         point_3rd = (right_eye_x, left_eye_y)
-        direction = -1 #rotate same direction to clock
+        direction = -1  # rotate same direction to clock
     else:
         point_3rd = (left_eye_x, right_eye_y)
-        direction = 1 #rotate inverse direction of clock
+        direction = 1  # rotate inverse direction of clock
 
-    #-----------------------
-    #find length of triangle edges
+    # -----------------------
+    # find length of triangle edges
 
     a = np.linalg.norm(np.array(left_eye) - np.array(point_3rd))
     b = np.linalg.norm(np.array(right_eye) - np.array(point_3rd))
     c = np.linalg.norm(np.array(right_eye) - np.array(left_eye))
 
-    if b != 0 and c != 0: #this multiplication causes division by zero in cos_a calculation
-        cos_a = (b*b + c*c - a*a)/(2*b*c)
-        
-        #PR15: While mathematically cos_a must be within the closed range [-1.0, 1.0], floating point errors would produce cases violating this
-        #In fact, we did come across a case where cos_a took the value 1.0000000169176173, which lead to a NaN from the following np.arccos step
-        cos_a = min(1.0, max(-1.0, cos_a))
-        
-        
-        angle = np.arccos(cos_a) #angle in radian
-        angle = (angle * 180) / math.pi #radian to degree
+    if b != 0 and c != 0:
+        cos_a = (b * b + c * c - a * a) / (2 * b * c)
 
-        #-----------------------
-        #rotate base image
+        cos_a = min(1.0, max(-1.0, cos_a))
+
+        angle = np.arccos(cos_a)
+        angle = (angle * 180) / math.pi
+
+        # -----------------------
+        # rotate base image
 
         if direction == -1:
             angle = 90 - angle
@@ -85,15 +76,12 @@ def img_crop(img_path):
 
         x1 = max(0, int(cx - crop_size / 2))
         x2 = int(cx + crop_size / 2)
-        y1 = max(0, int(cy - crop_size *0.6))
-        y2 = int(cy + crop_size *0.4)
+        y1 = max(0, int(cy - crop_size * 0.6))
+        y2 = int(cy + crop_size * 0.4)
 
         face_img = rotated_img[y1:y2, x1:x2]
 
-        return face_img
-    
-    
-pic_path = 'imgs/Jenny.jpg'
-crop_img = Image.fromarray(img_crop(pic_path))
-crop_img.save("imgs/crop_Jenny.jpg")
-#crop_img.save(f'imgs/crop_{ntpath.basename(file_path)}')
+        # Save cropped image
+        cropped_image = Image.fromarray(face_img)
+        cropped_image.save(f'{output_folder}/crop_{ntpath.basename(input_path)}')
+
